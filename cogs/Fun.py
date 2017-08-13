ac36cf728287df36cf795fe34b4718779779bca3
skielred <skielred@gmail.com>
2017-08-13 19:04:39 +0200

    Fixed contexT ->  context

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -46,7 +46,7 @@ class Fun:
     @commands.command(pass_context=True, aliases=['ri', 'riz', 'regional_indicator'])
     async def emoji(self, context):
         content = context.message.content[(len(context.prefix + context.invoked_with)) + 1:]
-        if content in [None, '', ' '] or contexT.invoked_with == 'riz' and not is_owner(context):
+        if content in [None, '', ' '] or context.invoked_with == 'riz' and not is_owner(context):
             return
         msg = ''
         if context.invoked_with == 'ri':
