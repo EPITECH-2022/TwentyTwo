0e27445fa301bc66e0f5139012319231e28a33c4
skielred <skielred@gmail.com>
2017-08-13 18:06:39 +0200

    Revised age Fun command and added joined Fun command

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import discord
 from discord.ext import commands
 
@@ -23,8 +25,17 @@ class Fun:
         ''' Tells the age of a Discord account '''
         if member is None:
             member = context.message.author
-        await self.bot.say('`{0}` joined Discord at {0.created_at}'.format(member))
+        msg = '`{}` joined Discord on {}'
+        await self.bot.say(msg.format(member, datetime.strftime(member.created_at, '%d %B %Y at %X')))
 
-    @commands.command()
-    async def help(self):
+    @commands.command(pass_context=True)
+    async def joined(self, context, member: discord.Member = None):
+        ''' Tells the age of a Discord account '''
+        if member is None:
+            member = context.message.author
+        msg = '`{}` joined *{}* on {}'
+        await self.bot.say(msg.format(member, datetime.strftime(member.joined_at, member.server, '%d %B %Y at %X')))
+
+    @commands.command(name='help')
+    async def _help(self):
         await self.bot.reply('ask `Tina#4153`')
